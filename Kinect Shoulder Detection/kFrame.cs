3068ebf4678504511a719692b45fb39388fa3de3
coch9894 <coch9894@vandals.uidaho.edu>
2013-10-15 11:05:55 -0700

    Skeletal Tracking

--- Kinect Shoulder Detection/kFrame.cs
+++ Kinect Shoulder Detection/kFrame.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Microsoft.Samples.Kinect.ColorBasics
+namespace Microsoft.Samples.Kinect.SkeletonBasics
 {
     public class kFrame
     {
